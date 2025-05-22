163b24a5be6cbfa119cbf5829b3b5b77c72af905
ravindravp <ravindravpauskar@gmail.com>
2025-05-22 15:18:03 +0530

    Update and rename checkout.groovy to readme.md

--- vars/checkout.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map scmConfig) {
-    if (!scmConfig) {
-        error "SCM configuration is required for checkout"
-    }
-    // Use Jenkins' built-in checkout step
-    checkout scmConfig
-}
